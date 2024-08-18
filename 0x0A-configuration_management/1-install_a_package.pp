0cb74c6fa641c640f19f6c626cceea208400941c
@Aker120112 <ezekieltaiwo232@gmail.com>
2024-08-17 20:40:39 -0700

    corrected

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
-# puppet declarative script to install flask from pip3.
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
+#!/usr/bin/env bash
+# Install puppet-lint
+
+package { 'puppet-lint':
+    ensure   => '2.1.1',
+    provider => 'gem',
+  }
